b945df58d628d22bfa3b1425f97f5f8a5fc4f92c
Sungmin Kim <chefminkim58@gmail.com>
2022-01-24 08:47:58 -0500

    fixated voltage scale on LM35 temp sensor

--- Arduino_Practice_Examples/Thermometer_project.ino
+++ Arduino_Practice_Examples/Thermometer_project.ino
@@ -10,27 +10,30 @@ void setup() {
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
+  analogReference(1.1); //sets full scale from 5V to 1.1V
 }
 
 void loop() {
 
     temp = analogRead(A0);
-    temp = temp * 4.88 //converts adc to voltage
-    temp = temp/10 //fixates 10mv/Cº to mv/Cº
+    temp = temp * 1.1/1023 //scales based on full scale
+    temp = temp/10; //fixates 10mv/Cº to mv/Cº
 
+    Serial.println("Temperature in celcius is: " + temp);
+    
     if (temp < 10) {
         red = 0;
         green = 0;
         blue = 255;
     }
 
-    if (temp < 25 && temp > 10) {
+    if (temp < 30 && temp > 10) {
         red = 0;
         green = 255;
         blue = 0;
     }
 
-    if (temp > 25) {
+    if (temp > 30) {
         red = 255;
         green = 0;
         blue = 0;
